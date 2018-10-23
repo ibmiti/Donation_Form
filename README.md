bfb5cb8fa0846bb570e0f365a405ff3d2dda456f
Dimitri_McD <devebeme@gmail.com>
2018-10-22 20:43:48 -0400

    commit all files

---
+++ README.md
@@ -0,0 +1,11 @@
+
+<!--  first thing we need to do  is create  two cards
+
+      the first card needs to be contained
+
+      card contained now we need to write the content inside of card2
+
+      then create a button
+
+      then create a text area
+ -->
---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
